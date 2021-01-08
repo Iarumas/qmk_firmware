adb91029848d07b3e7de74b9efe43f9d7ac754da
U-DESKTOP-N8P996V\Samurai <juergen.schrall@web.de>
2021-01-08 19:32:26 +0100

    Some small changes before everything is lost again

--- keyboards/keebio/levinson/keymaps/iarumas/rules.mk
+++ keyboards/keebio/levinson/keymaps/iarumas/rules.mk
@@ -1,17 +1,3 @@
-#### Defaults for Levinson ####
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-#   Teensy       halfkay
-#   Pro Micro    caterina
-#   Atmel DFU    atmel-dfu
-#   LUFA DFU     lufa-dfu
-#   QMK DFU      qmk-dfu
-#   ATmega32A    bootloadHID
-#   ATmega328P   USBasp
-BOOTLOADER = caterina
-
 #### own configuration
 COMMAND_ENABLE = no # override of Levinson default
 
@@ -19,6 +5,6 @@ SPLIT_KEYBOARD = yes
 RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = yes
 
-AUTO_SHIFT_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
 
 MOUSEKEY_ENABLE = yes
